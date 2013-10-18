3cad3821fff5ccadb7cf812b16ee59457152f269
Dan Millar <dan@decode.uk.com>
2013-10-18 22:31:15 +0100

    Update watch files

--- gruntfile.js
+++ gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     watch: {
-        files: ['**/*'],
+        files: ['**/*.css', '**/*.js'],
         tasks: ['default']      
     }
   });
